6c5920f7f054407330e8ce3509b11e703ed4dc4b
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-09-21 16:00:20 -0300

    finalizado 01

---
+++ src/01/index.ts
@@ -0,0 +1,30 @@
+const enderco = {
+  rua: "10",
+  numero: 5,
+  complemento: "Quadra 20 Lt 25",
+  bairro: "Alameda",
+  cep: "75084-855",
+  cidade: "São Paulo",
+  estado: "SP",
+  pais: "Brasil",
+};
+
+interface TelefoneInterface {
+  tipo: "cel" | "res" | "com";
+  numeroTelefone: string;
+}
+
+const telefone: TelefoneInterface = {
+  tipo: "cel",
+  numeroTelefone: "(62) 99568-9638",
+};
+
+const usuario = {
+  nome: "Gilson Alves",
+  email: "gilsonalves@gmail.com",
+  telefone,
+  data_nascimento: "17/08/2000",
+  enderco,
+};
+
+console.log(usuario);
