360e8ba13c02e4516359b0594b03af87dec347e1
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-09-21 17:30:35 -0300

    correcao exercicos 04

--- src/04/index.ts
+++ src/04/index.ts
@@ -30,13 +30,13 @@ const usuarios = [
 const maior_idade = 17;
 
 const verificaMaiorIdade = (usuarios: UsuarioInterface04[]) => {
-  const newUsuarios = [...usuarios];
   for (const [index, usuario] of usuarios.entries()) {
     usuario.idade > maior_idade
-      ? (newUsuarios[index].maior_idade = true)
-      : (newUsuarios[index].maior_idade = false);
+      ? (usuarios[index].maior_idade = true)
+      : (usuarios[index].maior_idade = false);
   }
-  return newUsuarios;
 };
 
-console.log(verificaMaiorIdade(usuarios));
+verificaMaiorIdade(usuarios);
+
+console.log(usuarios);
---
+++ src/05/index.ts
@@ -0,0 +1,39 @@
+// interface AulaInterface {
+//   identificador: number;
+//   nome_da_aula: string;
+// }
+
+// interface CursoInterface {
+//   id: number;
+//   nome: string;
+//   aulas: AulaInterface[];
+// }
+
+// const aulas: AulaInterface[] = [
+//   {
+//     identificador: 1,
+//     nome_da_aula: "Introdução a programação",
+//   },
+//   {
+//     identificador: 2,
+//     nome_da_aula: "Variáveis",
+//   },
+//   {
+//     identificador: 3,
+//     nome_da_aula: "Condicionais",
+//   },
+//   {
+//     identificador: 4,
+//     nome_da_aula: "Arrays",
+//   },
+// ];
+
+// const cadastrarAulas = ({ id, nome, aulas }: CursoInterface): CursoInterface => {
+//   return {
+//     id,
+//     nome,
+//     aulas: [...aulas],
+//   };
+// };
+
+// console.log(cadastrarAulas({1234; "Lógica de programação" aulas}));
